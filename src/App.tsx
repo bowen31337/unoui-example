7e7f715771b7418276f8af6cb99993e720ad2531
Bowen Li <bowen31337@gmail.com>
2022-01-08 18:46:35 +1100

    atom components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, Text } from "@unoui/atoms";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box bg="red" m={5} p={3}>
+        This is a Box from UnoUi
+      </Box>
+      <Text as='p'>
+        This is a text
+      </Text>
     </div>
   );
 }
